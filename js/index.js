10a49641ecdd02306819edab8262f73020e87d28
Priscilla-22 <priscillamw123@gmail.com>
2024-01-09 08:04:14 +0300

    feat: add functionality to fetch user repositories

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-const search = document.querySelector('#search');
-// search.innerText = data.search;
+let selectedName = null;
 
+const search = document.querySelector('#search');
 const submitBtn = document.querySelector(`input[type='submit']`);
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   handleSubmitBtnClick();
 });
+
+//fetch user details
 function handleSubmitBtnClick() {
   const userName = search.value;
 
@@ -26,15 +28,23 @@ function handleSubmitBtnClick() {
       userInfo.items.forEach((user) => {
         const listItems = document.createElement('li');
 
+        //login name
         const loginName = document.createElement('p');
         loginName.innerHTML = `<strong>login Name:</strong> ${user.login}`;
         listItems.appendChild(loginName);
 
+        loginName.addEventListener('click', (e) => {
+          e.preventDefault();
+          getUserRepos(user.login);
+        });
+
+        //avatar image
         const avatarImg = document.createElement('img');
         avatarImg.src = user.avatar_url;
         avatarImg.alt = 'Avatar';
         listItems.appendChild(avatarImg);
 
+        //profile
         const profileLink = document.createElement('a');
         profileLink.href = user.html_url;
         profileLink.textContent = 'View profile';
@@ -43,7 +53,34 @@ function handleSubmitBtnClick() {
         userList.appendChild(listItems);
       });
     })
-    .catch((error) => {
-      console.error('Error fetching GitHub users:', error);
+    .catch((err) => {
+      console.error('Error fetching GitHub users:', err);
+    });
+}
+
+//fetch user repos
+function getUserRepos(login) {
+  selectedName = login;
+
+  fetch(`https://api.github.com/users/${selectedName}/repos`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github.v3+json',
+    },
+  })
+    .then((resp) => resp.json())
+    .then((repos) => {
+      const repoList = document.querySelector('#repos-list');
+      repoList.innerHTML = '';
+
+      repos.forEach((repo) => {
+        const eachRepoLink = document.createElement('li');
+        eachRepoLink.textContent = repo.name;
+        repoList.appendChild(eachRepoLink);
+      });
+    })
+    .catch((err) => {
+      console.error('Error fetching user repositories', err);
     });
 }
